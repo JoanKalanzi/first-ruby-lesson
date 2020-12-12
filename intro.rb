b253cea47e6ca547214b6625d4e6885c28e4062c
NaomiBid <naomi0bid@gmail.com>
2020-12-12 11:58:04 +0000

    exploring ruby strings and numbers

---
+++ intro.rb
@@ -0,0 +1,9 @@
+puts "hello here"
+puts "today is friday"
+puts "12" + "5"
+puts 12 + 5
+puts "scented" + " " +"candles"
+puts "i" + " " + "want" + " " + "to" + " " + "go" + " " + "cycling"
+puts 100-5
+puts 10*5
+puts 10/5
